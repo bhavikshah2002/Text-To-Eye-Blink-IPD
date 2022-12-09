cb7004496c6eff3222384f636b5d3896b4f43fc8
Bhavik Shah <80097065+bhavikshah2002@users.noreply.github.com>
2022-12-09 18:34:39 +0530

    GUI Changed as per requirements

--- eyes.py
+++ eyes.py
@@ -5,29 +5,38 @@ FONT_NAME = "Courier"
 c = morsecodetotext2.texttomorsecode()
 is_true=True
 timer=None
-ans=[]
+ans=['/']
+whole_word=' '
+show_word=''
 cur_i=0
 cur_j=0
 def morse_to_blink(i):
     global isLongBlink,timer
     # print(f"BlinkHere {i}")
     if i=="-":
-        isLongBlink=True
+        isLongBlink=2000
+    elif i==".":
+        isLongBlink=1000
     else:
-        isLongBlink=False
+        isLongBlink=3000
     window.after_cancel(timer)
     timer = window.after(20, Blink1)
 
 def all_Code(i):
-    global ans,timer
+    global ans,timer,show_word,whole_word
     # print(f"allIndex {i}")
     if(i<len(ans)):
         window.after_cancel(timer)
         timer = window.after(20, singleCode,i,0)
+    else:
+        ans=['/']
+        show_word=''
+        whole_word=' '
+
     
 
 def singleCode(i,j):
-    global ans,timer,cur_i,cur_j
+    global ans,timer,cur_i,cur_j,show_word,show_label
     cur_i=i
     cur_j=j
     if(j<len(ans[i])):
@@ -35,17 +44,24 @@ def singleCode(i,j):
         window.after_cancel(timer)
         timer = window.after(50, morse_to_blink,ans[i][j])
     else:
+        show_word+=whole_word[len(show_word)]
+        print(show_word)
+        show_label.config(text=f"Letter :{show_word}")
         window.after_cancel(timer)
         timer = window.after(1000, all_Code,i+1)
 
 def on_enter_button_click():
-    global ans,timer
+    global ans,whole_word,show_word
     txt = text_str.get(1.0, "end-1c")
     text_str.delete(1.0, "end")
     txt = txt.lower()
+    show_word=''
+    whole_word=txt
     ans = c.text_to_morse(txt)
-    timer = window.after(20, all_Code,0)
 
+def on_start_button_click():
+    global timer
+    timer = window.after(20, all_Code,0)
         
 
 window = Tk()
@@ -56,7 +72,7 @@ canvas = Canvas(width=500, height=400, highlightthickness=0, background="grey")
 canvas.grid(row=1, column=1)
 
 # Time Of Blink
-isLongBlink=True
+isLongBlink=2000
 
 
 # Creating Eye
@@ -84,13 +100,23 @@ t15=createLeftEye(195,195)
 t25=createRightEye(195,195)
 
 
-heading = Label(text="Enter a text Here:", font=(FONT_NAME, 50, "bold"))
+heading = Label(text="Text to eye blink", font=(FONT_NAME, 50, "bold"))
 heading.grid(row=0, column=5)
+
+entry_label = Label(text="Enter a text: ", font=(FONT_NAME, 32, "normal"))
+entry_label.place(x=650,y=115)
 
 # text_str = Entry(width=50)
 text_str = Text(window, height=10, width=60, font=(FONT_NAME, 12, "normal"))
 text_str.grid(row=1, column=5, columnspan=2)
 text_str.focus_set()
+
+show_label = Label(text=f"Letter :{show_word}", font=(FONT_NAME, 24, "normal"))
+show_label.place(x=550,y=400)
+
+start_button = Button(text="Start", background='grey',  highlightthickness=0, height=3, width=15,
+                      font=(FONT_NAME, 12, "bold"), command=on_start_button_click)
+start_button.place(x=170,y=500)
 
 enter_button = Button(text="Enter", background='grey',  highlightthickness=0, height=3, width=15,
                       font=(FONT_NAME, 12, "bold"), command=on_enter_button_click)
@@ -141,10 +167,7 @@ def Blink3():
     timer= window.after(50, Blink4) 
 def Blink4():
     global timer, isLongBlink
-    if isLongBlink:
-        blinkTime=2000
-    else:
-        blinkTime=1000
+    blinkTime=isLongBlink
     window.after_cancel(timer)
     window.after(50, Blink_It,3)
     timer= window.after(blinkTime, Open4) 
